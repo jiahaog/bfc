b49352b6d7ee6197eec6a59adbf9ce50811b4700
Jia Hao Goh <jiahaog@users.noreply.github.com>
2022-06-19 10:09:57 +0800

    Entrypoint accepts a path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use std::io::{stdin, stdout, Read, Write};
+use std::{
+    env,
+    fs::read_to_string,
+    io::{stdin, stdout, Read, Write},
+};
 
 fn main() {
-    let ops = parse("+[,.]").unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Unexpected number of args provided. Should only receive one arg as a path to a .bf file");
+    }
+
+    let program = read_to_string(args.get(1).unwrap()).unwrap();
+
+    let ops = parse(&program).unwrap();
 
     run(&mut stdin(), &mut stdout(), ops).unwrap();
 }
